1440a8c2aca73053feb66c41707b6b2894ecfb8c
Almedina Hadrovic <almedinahadrovic5@gmail.com>
2025-11-28 10:15:26 +0100

    Objasnjenje prostih tipova-decimalna vrijednost

---
+++ src/main/java/com/itakademija/thre/DecimalNumberDemo.java
@@ -0,0 +1,20 @@
+package com.itakademija.thre;
+
+public class DecimalNumberDemo {
+   public static void main(String[] args) {
+       float floatNumber1=(float) 3.4;
+       System.out.println(floatNumber1);
+       float floatNumber2= 3.4F;
+       System.out.println(floatNumber2);
+       double doubleNumber=3.4;
+       System.out.println(doubleNumber);
+       double a=0.1;
+       double b = 0.2;
+       double c = a+b;
+       System.out.println(a+b);
+       System.out.println(c);
+
+
+
+    }
+}
