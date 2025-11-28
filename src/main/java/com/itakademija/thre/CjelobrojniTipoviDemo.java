7b71b54f03564ff756a0bf9e43423b6c0f2a69b3
Almedina Hadrovic <almedinahadrovic5@gmail.com>
2025-11-28 09:22:16 +0100

    Objasnjenje prostih tipova-cijeli brojevi

---
+++ src/main/java/com/itakademija/thre/CjelobrojniTipoviDemo.java
@@ -0,0 +1,16 @@
+package com.itakademija.thre;
+
+public class CjelobrojniTipoviDemo {
+   public static void main(String[] args) {
+       byte firstNumber=127;
+       System.out.println(firstNumber);
+       short shortNumber=32767;
+       System.out.println(shortNumber);
+       int thirdNumber=128;
+       System.out.println(thirdNumber);
+       long longNumber=thirdNumber;
+       System.out.println(longNumber);
+
+
+    }
+}
---
+++ src/main/java/com/itakademija/two/Program.java
@@ -0,0 +1,10 @@
+package com.itakademija.two;
+
+public class Program {
+   public static void main(String[] args) {
+       System.out.println("Komanda za ispis");
+
+
+
+    }
+}
