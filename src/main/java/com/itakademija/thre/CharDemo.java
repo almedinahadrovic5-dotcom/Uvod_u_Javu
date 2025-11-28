8d9780c4e3bd49dcc7e7f4691a3f7107d7ed1edb
Almedina Hadrovic <almedinahadrovic5@gmail.com>
2025-11-28 10:17:31 +0100

    Objasnjenje prostih tipova-jedan znak sa tastature(karakter)

---
+++ src/main/java/com/itakademija/thre/CharDemo.java
@@ -0,0 +1,17 @@
+package com.itakademija.thre;
+
+public class CharDemo {
+   public static void main(String[] args) {
+       char charVar='K';
+       System.out.println(charVar);
+       char anothercharVar='?';
+       System.out.println(anothercharVar);
+       char thirdCharVar=65;
+       System.out.println(thirdCharVar);
+       int intNumber=thirdCharVar;
+       System.out.println(intNumber);
+
+
+
+    }
+}
